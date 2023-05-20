c2a7530ed663bb8ab8fc60f4517cb8f36082229a
KishAndSur <133739495+KishAndSur@users.noreply.github.com>
2023-05-20 08:35:37 +0300

    Add files via upload

---
+++ shercterni (11)/shercterni (11)/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace shercterni__11_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.picFrame = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picFrame
+            // 
+            this.picFrame.Location = new System.Drawing.Point(12, 79);
+            this.picFrame.Name = "picFrame";
+            this.picFrame.Size = new System.Drawing.Size(535, 364);
+            this.picFrame.TabIndex = 1;
+            this.picFrame.TabStop = false;
+            this.picFrame.Click += new System.EventHandler(this.picFrame_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(167, 12);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(380, 45);
+            this.trackBar1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 455);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.picFrame);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picFrame;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ shercterni (11)/shercterni (11)/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shercterni__11_
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap[] Frames;
+        private int FrameNum = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Frames = new Bitmap[18];
+            for (int i = 0; i < 18; i++)
+            {
+                Frames[i] = new Bitmap("Frame" + i + ".png");
+            }
+
+            picFrame.Image = Frames[FrameNum];
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            FrameNum = ++FrameNum % Frames.Length;
+            picFrame.Image = Frames[FrameNum];
+        }
+        private void picFrame_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            if (timer1.Enabled) button1.Text = "Стоп";
+            else button1.Text = "Старт";
+        }
+
+       
+
+
+    }
+}
